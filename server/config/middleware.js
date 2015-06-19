74f6767b42f04006091d1d2218e4ebeeefd20553
Bahia El-Sharkawy <bahia.sharkawy@gmail.com>
2015-06-19 15:57:11 -0700

    (feat) Create initial server structure
    Created a server and client directory, and an initial index.js
    that serves static assets.

---
+++ server/config/middleware.js
@@ -0,0 +1,10 @@
+
+
+module.exports = function(app, express){
+  
+  app.use(express.static(__dirname + '/../../client'));
+  
+}
+
+
+
---
+++ server/server.js
@@ -0,0 +1,10 @@
+/********************** require all packages*******************/
+var express = require('express');
+
+
+/********************* initialization ************************/
+var app = express();
+require('./config/middleware.js')(app, express);
+
+
+module.exports = app;
