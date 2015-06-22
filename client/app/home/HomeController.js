2a62045991aea1b17756caad8c7017d7dcc0ac68
JD <jdstep23@gmail.com>
2015-06-22 10:52:37 -0700

    added values for algorithm and changed font sized in text area

--- client/app/home/HomeController.js
+++ client/app/home/HomeController.js
@@ -1,2 +1,16 @@
 
-app.controller('HomeController', )
+app.controller('HomeController', ['$scope', function($scope) {
+  $scope.timerInput = 15;
+
+  $scope.currentFlows = [];
+
+  $scope.currentPotentialMinute;
+
+  $scope.currentPotentialSession;
+
+  $scope.currentFlowMinute;
+
+  $scope.currentFlowSession;
+
+
+}]);
