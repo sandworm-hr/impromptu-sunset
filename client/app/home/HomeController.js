e4aeb9bc2c2f5c9bfc8f707ffb4546092f525493
Peter Espe <peterlucasespe@gmail.com>
2015-06-22 12:55:57 -0700

    (feat) add score calculator

--- client/app/home/HomeController.js
+++ client/app/home/HomeController.js
@@ -1,12 +1,32 @@
 
-app.controller('HomeController', ['$scope', function($scope) {
+app.controller('HomeController', ['$scope', function($scope, $interval) {
 
-  $scope.lastTime = 0;
+  $scope.lastTime = 1;
 
   $scope.setTime = function(event){
     $scope.lastTime = event.timeStamp;
   }
 
+  var getTime = function() {
+    var date = new Date();
+    return date.getTime();
+  }
+
+  var getScore = function() {
+    var score = 10000;
+    var diff = getTime() - $scope.lastTime;
+
+    if (diff > 2000 && diff <= 12000) {
+      score -= (diff - 2000);
+    } else if (diff > 12000) {
+      score = 1;
+    }
+    $scope.totalFlow += score;
+    console.log(score);
+  }
+
+  // $interval(getScore, 1000, 0);
+  
   $scope.timerInput = 15;
 
   $scope.Flows = [4, 8, 10, 30];
@@ -15,13 +35,16 @@ app.controller('HomeController', ['$scope', function($scope) {
 
   $scope.FlowSession = 70;
 
-  $scope.PotentialMinute = 5;
+  $scope.totalFlow = 0;
 
   $scope.FlowMinute = 3;
 
-  $scope.startTimer = function() {
-    console.log('starting the timer');
-  };
+  $scope.startTime;
 
+  $scope.startTimer = function() {
+    $scope.startTime = getTime();
+    console.log('starting the timer');
+    setInterval(getScore, 1000);
+  };
 
 }]);
