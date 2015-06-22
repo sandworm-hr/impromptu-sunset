8bfc4f58e9fcc7e099059a8298db81c9ffdd9575
Peter Espe <peterlucasespe@gmail.com>
2015-06-22 14:16:53 -0700

    (feat) implement scorekeeping w/ timer

--- client/app/home/HomeController.js
+++ client/app/home/HomeController.js
@@ -1,16 +1,20 @@
 
-app.controller('HomeController', ['$scope', function($scope, $interval) {
+app.controller('HomeController', ['$scope', '$interval', function($scope, $interval) {
 
   $scope.lastTime = 1;
 
   $scope.setTime = function(event){
     $scope.lastTime = event.timeStamp;
-  }
+  };
 
   var getTime = function() {
     var date = new Date();
     return date.getTime();
-  }
+  };
+
+  var updateMinute = function() {
+    $scope.minute = Math.floor((getTime() - $scope.startTime) / 60000);
+  };
 
   var getScore = function() {
     var score = 10000;
@@ -21,30 +25,49 @@ app.controller('HomeController', ['$scope', function($scope, $interval) {
     } else if (diff > 12000) {
       score = 1;
     }
-    $scope.totalFlow += score;
-    console.log(score);
-  }
 
-  // $interval(getScore, 1000, 0);
-  
+    updateMinute();
+
+    $scope.scores[$scope.minute] += score;
+    $scope.latestScore = score;
+
+    console.log(score);
+  };
+
+
   $scope.timerInput = 15;
 
-  $scope.Flows = [4, 8, 10, 30];
+  $scope.scores = [4, 8, 10, 30];
 
-  $scope.PotentialSession = 100;
+  $scope.potentialSession = 0;
 
-  $scope.FlowSession = 70;
+  $scope.latestScore = 0;
 
-  $scope.totalFlow = 0;
+  $scope.totalScore = 0;
 
-  $scope.FlowMinute = 3;
+  $scope.minute = 3;
 
   $scope.startTime;
 
   $scope.startTimer = function() {
-    $scope.startTime = getTime();
-    console.log('starting the timer');
-    setInterval(getScore, 1000);
+    // $interval.cancel(getScore);
+    $scope.timerInput = parseInt($scope.timerInput);
+    if ($scope.timerInput) {
+      console.log('starting the timer');
+
+      $scope.potentialSession = $scope.timerInput * 60 * 10000;
+      $scope.startTime = getTime();
+
+      $interval(getScore, 1000, 0);
+
+      $scope.scores = [];
+      for (var i = 0; i < $scope.timerInput; i++) {
+        $scope.scores.push(0);
+      };
+
+    } else {
+      console.log('invalid time');
+    } 
   };
 
 }]);
