e8b4300f5df3b68bc63e261b0b329dcb7e1ee726
JD <jdstep23@gmail.com>
2015-06-22 11:16:00 -0700

    fixed naming on debug values

--- client/app/home/HomeController.js
+++ client/app/home/HomeController.js
@@ -2,15 +2,15 @@
 app.controller('HomeController', ['$scope', function($scope) {
   $scope.timerInput = 15;
 
-  $scope.currentFlows = [4, 8, 10, 30];
+  $scope.Flows = [4, 8, 10, 30];
 
-  $scope.currentPotentialSession = 100;
+  $scope.PotentialSession = 100;
 
-  $scope.currentFlowSession = 70;
+  $scope.FlowSession = 70;
 
-  $scope.currentPotentialMinute = 5;
+  $scope.PotentialMinute = 5;
 
-  $scope.currentFlowMinute = 3;
+  $scope.FlowMinute = 3;
 
   $scope.startTimer = function() {
     console.log('starting the timer');
