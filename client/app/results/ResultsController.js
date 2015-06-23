769a21bf1bd5e203806f4f5d4c0740574d956df6
Peter Espe <peterlucasespe@gmail.com>
2015-06-23 11:03:52 -0700

    (bug) fix dependency injection for results

--- client/app/results/ResultsController.js
+++ client/app/results/ResultsController.js
@@ -1,1 +1,10 @@
-Hello World
+
+app.controller('ResultsController', ['$scope', 'Results', function($scope, Results) {
+
+  // console.log(Results);
+
+  // $scope.duration = Results.getDuration();
+  // $scope.scores = Results.getScores();
+  // $scope.rawText = Results.getText();
+
+}]);
