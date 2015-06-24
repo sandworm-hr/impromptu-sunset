c124e5db32d3c9bf07e05981b6c8d5fc4878b503
Bahia El-Sharkawy <bahia.sharkawy@gmail.com>
2015-06-23 18:30:14 -0700

    (feat) Need to login to see home and profile page.

--- client/app/config/config.js
+++ client/app/config/config.js
@@ -30,8 +30,51 @@ app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
       url: '/results',
       templateUrl: 'app/results/results.html'
     })
-});
+})
+.run(['$http', '$rootScope','$cookies','$state','Session', function($http, $rootScope, $cookies,$state,Session) {
 
+    var statesThatDontRequireAuth = ['login', 'signup'];
+    var statesThatRequireNoAuth = ['login', 'signup'];
+
+    //check if route requires no auth
+    var stateNoAuth = function(state) {
+        for (var element in statesThatRequireNoAuth) {
+            var input = statesThatRequireNoAuth[element];
+            if (state.substring(0, input.length) == input)
+                return true;
+        }
+        return false;
+    }
+
+    // check if route does not require authentication
+    var routeClean = function(state) {
+        for (var element in statesThatDontRequireAuth) {
+            var input = statesThatDontRequireAuth[element];
+            if (state.substring(0, input.length) == input)
+                return true
+        }
+        return false;
+    }
+
+
+    $rootScope.$on('$stateChangeStart', function(ev, to, toParams, from, fromParams) {
+
+        var result= Session.isAuthenticated();
+        
+        if (!routeClean(to.name) && result == false ) // user not logged in trying to access a page that needs authentication.
+        {
+            console.log("not logged in!!");
+            ev.preventDefault();
+            $state.go("login");
+        } else if (stateNoAuth(to.name)  && result == true) { //logged in but going to not logged in page
+            ev.preventDefault();
+            $state.go("index"); 
+        } 
+
+    });
+}]);
+
+        
 // use ngEnter="action" to trigger starting
 app.directive('ngEnter', function () {
     return function (scope, element, attrs) {
