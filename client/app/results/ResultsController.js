978b5c4aa4108df9ae110ea3e84eeae6fb7a0e95
Peter Espe <peterlucasespe@gmail.com>
2015-06-24 10:48:53 -0700

    (feat) append svg to DOM

--- client/app/results/ResultsController.js
+++ client/app/results/ResultsController.js
@@ -1,5 +1,5 @@
 
-app.controller('ResultsController', ['$scope', 'Results', function($scope, Results) {
+app.controller('ResultsController', ['$scope', '$timeout', 'Results', function($scope, $timeout, Results) {
 
 
     $scope.duration = Results.getDuration();
@@ -25,6 +25,42 @@ app.controller('ResultsController', ['$scope', 'Results', function($scope, Resul
 
       Results.postResults(resultsObj);
     };
+
+    $scope.createGraph = function() {
+
+        var margin = {top: 20, right: 20, bottom: 30, left: 50},
+          width = 960 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom;
+
+        var x = d3.scale.linear()
+          .range([0, width]);
+
+        var y = d3.scale.linear()
+          .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("left");
+
+        var line = d3.svg.line()
+          .x(function(d) { return x(d.close); })
+          .y(function(d) { return y(d.close); });
+
+        $timeout(function() {
+            console.log('appending svg');
+            var svg = d3.select("#graph").append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+          
+        }, 1);
+
+    }
 
 
   // $scope.debugSendValues = function(valuesObj) {
