0b8a8186a73b4794cfd3e64defc87d2620f00301
Tamara <tmwoodson@gmail.com>
2015-07-02 14:50:01 -0700

    Start adding edit mode

--- client/app/review/ReviewController.js
+++ client/app/review/ReviewController.js
@@ -9,29 +9,31 @@ app.controller('ReviewController', ['$scope', '$stateParams', 'Review', 'Session
   };
 
   $scope.saveComment = function() {
-    var comment = {};
-    comment.comment = $scope.commentInput;
-    comment.from = Session.getUser().userId;
-    comment.UserId = $scope.sessionInfo.UserId;
-    comment.SessionId = $scope.sessionInfo.id;
-    console.log(comment);
-    Review.postComment(comment)
-      .success(function(data, status) {
-         $scope.status = 'Saved Comment';
-         // clear out comment
-         // display some sort of success message
-      })
-      .catch(function(data) {
-      // FOR TESTING:
-      // in testing there is no data object
-      // if there is a data object, we are not running a test
-      // therefore we need to set the scope message
-      if (data.data) {
-        $scope.message = data.data.message;
-      }
-      $scope.status = 'Save Failed';
-     });
+    if ($scope.commentInput !== '') {
+      var comment = {};
+      comment.comment = $scope.commentInput;
+      comment.from = Session.getUser().userId;
+      comment.UserId = $scope.sessionInfo.UserId;
+      comment.SessionId = $scope.sessionInfo.id;
+      console.log(comment);
+      Review.postComment(comment)
+        .success(function(data, status) {
+           $scope.commentInput = '';
+           // TO DO: display some sort of success message
+        })
+        .catch(function(data) {
+          $scope.status = 'Save Failed';
+       });
+    }
   };
+
+  $scope.editMode = function() {
+    if ($scope.mode === 'view') {
+      $scope.mode = 'edit';
+    }
+  };
+
+  $scope.mode = "view";
 
 
   if ($stateParams.id) {
