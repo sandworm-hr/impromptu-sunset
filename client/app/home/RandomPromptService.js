e4926c432a8244f3dd0cc2035c45ddf706232e5e
Paul Sokolik <paul.sokolik@gmail.com>
2015-07-02 19:21:48 -0700

    add reddit writing prompts random prompt option

---
+++ client/app/home/RandomPromptService.js
@@ -0,0 +1,29 @@
+angular.module('app.services')
+  .factory('RandomPrompt', function ($http) {
+    var services = {};
+
+    services.getPrompt = function(callback) {
+      $http({
+        method: 'GET',
+        url: 'http://www.reddit.com/r/writingprompts/.json'
+      })
+      .success(function(response) {
+        var check = false;
+        while(!check) {
+          var prompts = response.data.children;
+          var index = Math.floor(Math.random()*prompts.length);
+          var title = prompts[index].data.title;
+          if(title.slice(0,4) === '[WP]' || title.slice(0,4) === '[TT]' || title.slice(0,4) === '[EU]') {
+            var randomPrompt = title.slice(4).trim();
+            check = true;
+          }  
+        }
+        callback(randomPrompt);
+      })
+      .error(function(error) {
+        console.log(error);
+      });
+    };
+
+    return services;
+  });
