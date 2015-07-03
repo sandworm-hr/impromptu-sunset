dbf7d43e6a1c12c8bf7f6545405b2f71ea491316
Tamara <tmwoodson@gmail.com>
2015-07-03 12:07:07 -0700

    Delete console logs

--- client/app/profile/ProfileController.js
+++ client/app/profile/ProfileController.js
@@ -6,19 +6,14 @@ app.controller('ProfileController', ['$scope', 'Session', 'Sessions', '$statePar
 
   // Calls the Session factory to get the sessions of that user.
   $scope.getSessions = function (callback) {
-
     if ($stateParams.username) {
       Sessions.getSessions(function(data){
-        console.log('all sessions: ', data);
         $scope.sessions = $scope.filterSessions(data);
-        console.log('visibile sessions: ', $scope.sessions);
         callback(data);
       }, $stateParams.username);
     } else {
       Sessions.getSessions(function(data){
-        console.log('all sessions: ', data);
         $scope.sessions = $scope.filterSessions(data);
-        console.log('visible sessions: ', $scope.sessions);
         callback(data);
       });
     }
